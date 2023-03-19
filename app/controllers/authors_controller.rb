38f79cca635e77d53287b20b84b3fba88548949f
treva556 <marktreva92@gmail.com>
2023-03-19 20:34:16 +0300

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,16 +2,25 @@ class AuthorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    authors = Author.all 
-    render json: authors
+    authors = Author.all
+    render json: authors, include: ['profile', 'posts', 'posts.tags']
+  end
+  
+  def show
+    author = find_author
+    render json: author, include: ['profile', 'posts', 'posts.tags']
   end
 
-  def show
-    author = Author.find(params[:id])
-    render json: author
+  def profile
+    author = find_author
+    render json: author, serializer: AuthorProfileSerializer
   end
 
   private
+
+  def find_author
+    Author.find(params[:id])
+  end
 
   def render_not_found_response
     render json: { error: "Author not found" }, status: :not_found
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :tags, only: [:index, :show]
   resources :posts, only: [:index, :show]
   resources :authors, only: [:index, :show]
+
+  get '/authors/:id/profile', to: 'authors#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
